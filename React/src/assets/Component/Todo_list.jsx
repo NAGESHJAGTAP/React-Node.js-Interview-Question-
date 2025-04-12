351e5a994aaac3b3c363ee186cb70ac51c5ef591
Nageshjagtap <nageshjagtap063@gmail.com>
2025-04-12 22:40:59 +0530

    todo list

--- React/src/assets/Component/Todo_list.jsx
+++ React/src/assets/Component/Todo_list.jsx
@@ -51,3 +51,74 @@
 // }
 
 // export default TodoList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Todo List with Done
+function TodoListWithDone() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [doneTodos, setDoneTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleMarkDone = (index) => {
+    const doneItem = todos[index];
+    setDoneTodos([...doneTodos, doneItem]);
+    handleDelete(index);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button type='button' onClick={handleAddTodo}>Add Todo</button>
+      <h2>Pending Todos</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button type='button' onClick={() => handleMarkDone(index)}>Done</button>
+            <button type='button' onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Completed Todos</h2>
+      <ul>
+        {doneTodos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoListWithDone;
+
+
